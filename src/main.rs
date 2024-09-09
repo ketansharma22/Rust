f3e33518280f85fb3a706617d1bfb9cdb75d62cb
ketansharma22 <kanu220504@gmail.com>
2024-09-10 00:40:20 +0530

    loops and conditions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Ketan");
+    let mut sentence=0;
+    io(sentence);
+}
+fn io(mut sentence: i32){
+    for mut i in 20..40 {
+        if sentence+i % 2 ==0{
+            println!("{}",sentence+i);
+        }
+
+        i=i+1;
+    }
 }
